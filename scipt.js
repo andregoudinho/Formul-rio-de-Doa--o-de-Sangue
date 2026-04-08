6c20f231b0fc9b3b6607b7e456b07fbafa789957
andre_goudinho <andre_goudinho@estudante.sesisenai.org.br>
2026-04-07 21:20:45 -0300

    add - validações

--- scipt.js
+++ scipt.js
@@ -1,6 +1,6 @@
 let doadores = [];
 
-document.getElementById("formDoacao").addEventListener("submit", function(e){
+document.getElementById("formDoacao").addEventListener("submit", function (e) {
     e.preventDefault();
 
     let nome = document.getElementById("nome").value.trim();
@@ -12,6 +12,39 @@ document.getElementById("formDoacao").addEventListener("submit", function(e){
     let cidade = document.getElementById("cidade").value.trim();
     let estado = document.getElementById("estado").value.trim();
     let erro = "";
+
+    if (!nome.includes(" ")) {
+        erro = "Digite o nome e o sobrenome.";
+    } else if (!email.includes("@") || !email.includes(".")) {
+        erro = "Email inválido.";
+    } else if (idade < 16) {
+        erro = "Idade mínima é 16 anos.";
+    } else if (peso < 50) {
+        erro = "Peso mínimo é 50kg.";
+    } else if (tipoSanguineo === "") {
+        erro = "Selecione o tipo sanguíneo.";
+    } else if (!/^[0-9]+$/.test(telefone)) {
+        erro = "Telefone deve conter apenas números.";
+    } else if (
+        nome === "" || email === "" || cidade === "" || estado === ""
+    ) {
+        erro = "Preencha todos os campos.";
+    }
+    let mensagemErro = document.getElementById("mensagemErro");
+
+    if (erro !== "") {
+        mensagemErro.textContent = erro;
+        return;
+    }
+    mensagemErro.textContent = "";
+
+    let doador = {
+        nome: nome,
+        email: email,
+        idade: idade,
+        peso: peso,
+        tipoSanguineo: tipoSanguineo
+    }
 
 
 
